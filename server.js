1880959fa71be399bffabc2155476921229627b2
Frex Yang <xiang2g@hotmail.com>
2024-07-30 10:43:17 +0800

    fix

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 // 提供静态文件
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // 对于所有请求，发送 index.html
